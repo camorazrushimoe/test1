9adca791118fc5ace8de196df38127cb76ece5cf
Sergey Ryabushko <biginf.com@gmail.com>
2020-04-05 17:06:58 +0300

    Initial Commit

---
+++ Calculator2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Calculator2
+//
+//  Created by Sergey Ryabushko on 05.04.2020.
+//  Copyright © 2020 Sergey Ryabushko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Calculator2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Calculator2
+//
+//  Created by Sergey Ryabushko on 05.04.2020.
+//  Copyright © 2020 Sergey Ryabushko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
